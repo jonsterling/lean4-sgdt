ac84333e22e8b4cb1ac15dac38be91ffd98c0b6f
Jonathan Sterling <jon@jonmsterling.com>
2021-05-28 23:37:51 -0400

    continue

--- sgdt.lean
+++ sgdt.lean
@@ -1,31 +1,25 @@
 universe u
 
-def from_singleton {P : Prop} : Subtype (fun x : Unit => P) → P := by
-intro x
-cases x
-assumption
-
-def to_singleton {P : Prop} : P → Subtype (fun x : Unit => P) := by
-intro x
-constructor
-focus assumption
-focus constructor
-
-
 @[simp] constant Eq.mpr_mp_cancel : Eq.mpr (p : a = b) (Eq.mp (q : a = b) x) = x := by
 induction q
 simp [Eq.mpr,Eq.mp]
 
+@[simp] constant Eq.mp_mpr_cancel : Eq.mp (p : a = b) (Eq.mpr (q : a = b) x) = x := by
+induction q
+simp [Eq.mpr,Eq.mp]
 
-axiom ltr : Type u -> Type u
+
+axiom ltr : Sort u -> Sort u
 prefix:100 "▷" => ltr
 
 namespace ltr
-  variable {A B : Type u}
+  variable {A B : Sort u}
   axiom next : A → ▷ A
   axiom fix : (▷ A → A) → A
   axiom ap : ▷ (A → B) → ▷ A → ▷ B
   infixl:100 "⊛" => ap
+
+  axiom lex : ∀ x y : A, ▷ (x = y) = (next x = next y)
 
   namespace fix
     axiom red : {f : ▷ A → A} → fix f = f (next (fix f))
@@ -42,21 +36,20 @@ namespace ltr
     rw [← @red _ F]
     exact x
 
-    @[simp] def beta : (x : F (next (fix F))) → elim (intro x) = x := by
-    intro x
+    @[simp] def beta (x : F (next (fix F))) : elim (intro x) = x := by
     simp [intro,elim]
 
+    @[simp] def eta (x : fix F) : intro (elim x) = x := by
+    simp [intro,elim]
   end fix
 
-  namespace ap
-    @[simp] axiom red : ∀ {f : (A → B)} {x : A}, (next f) ⊛ (next x) = next (f x)
-  end ap
+  @[simp] axiom ap.red : ∀ {f : (A → B)} {x : A}, (next f) ⊛ (next x) = next (f x)
 end ltr
 
 axiom dltr : ▷ Type u -> Type u
 prefix:100 "[▷]" => dltr
 
-@[simp] axiom dltr.red : {A : Type u} → [▷] (ltr.next A) = ▷ A
+@[simp] axiom dltr.red : {A : Type u} → [▷] ltr.next A = ▷ A
 
 
 class domain (A : Type u) where
@@ -66,8 +59,11 @@ inductive sum (A B : Type u) :=
 | inl : A → sum A B
 | inr : B → sum A B
 
+macro "fix " p:term " => " d:term : term =>
+`(ltr.fix fun $p:term => $d:term)
+
 def lift (A : Type u) : Type u :=
-ltr.fix λ R =>
+fix R =>
 sum A ([▷] R)
 
 postfix:max "⊥" => lift
@@ -85,8 +81,7 @@ namespace lift
     step := lift.step
 
   noncomputable def bind [domain b] (f : a → b) : a⊥ → b :=
-  ltr.fix λ recbind => by
-  intro m
+  fix recbind => fun m => by
   match ltr.fix.elim m with
   | sum.inl x =>
     exact f x
@@ -100,19 +95,31 @@ namespace lift
   simp [bind, now]
   rw [ltr.fix.red]
   simp
+
+  def is_strict [domain a] [domain b] (f : a -> b) :=
+  ∀ x : ▷ a, f (domain.step x) = domain.step (ltr.next f ⊛ x)
+
+  constant bind.step.red [domain b] (f : a -> b) : is_strict (bind f) :=
+  fun x =>
+  fix rec => by
+  simp [bind,domain.step,step]
+  rw [ltr.fix.red]
+  simp [domain.step]
+  exact Eq.trans (by rw [ltr.fix.red]) (by simp)
+
 end lift
 
-noncomputable instance [domain a] [domain b] : domain (a × b) where
+noncomputable instance {a b : Type u} [domain a] [domain b] : domain (a × b) where
   step :=
-  λ p =>
-  ⟨domain.step $ ltr.next (λ x => x.1) ⊛ p,
-   domain.step $ ltr.next (λ x => x.2) ⊛ p⟩
+  fun p =>
+  ⟨domain.step $ ltr.next (fun x => x.1) ⊛ p,
+   domain.step $ ltr.next (fun x => x.2) ⊛ p⟩
 
 noncomputable instance [domain b] : domain (a → b) where
   step :=
-  λ f x =>
+  fun f x =>
   domain.step $
   f ⊛ ltr.next x
 
 noncomputable def domain.fix [domain a] (f : a -> a) : a :=
-ltr.fix $ f ∘ domain.step
+fix x => f $ domain.step x
